f46c2c642ffcb1fccb146ec669a559aaf0e32c39
barbara herrers <herrerabarbaraf@gmail.com>
2021-06-12 13:08:12 +0200

    proyect structure

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,7 +1,69 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/")
+@AllArgsConstructor
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    /**
+     * @return Get the entire object of a user
+     */
+    @GetMapping(path = "/user")
+    public String getUser() {
+        return "";
+    }
+
+    /**
+     * @param userId user to be delete
+     * @return You can delete user object through its ID
+     */
+    @DeleteMapping("/delete/user/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") String userId) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    /**
+     * @param user user to be modified
+     * @return response entity
+     */
+    @PutMapping("/update/user/{id}")
+    public ResponseEntity<User> update(@RequestBody User user) {
+       /* try {
+            if (userService.findOne(user.getEmail()) == null)
+                throw new UsernameNotFoundException("The email does not exists");
+            return ResponseEntity.ok(userService.update(user));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }*/
+        return ResponseEntity.badRequest().build();
+    }
+
+    /**
+     * @param user DTO object to create an user
+     * @return the registered user
+     */
+    @PostMapping("/sign-up/user")
+    public String registerUser(@RequestBody User user) {
+
+        //return registrationService.register(user);
+        return user.toString();
+    }
+    /*
+    ST7: POST /github/:username
+Descripción: Crea un GithubUserl, lo conecta con un usuario previamante creado a través de su id y guarda el User
+ST8: POST /gitlab/:username
+Descripción: Crea un GitlabUser, lo conecta con un usuario previamante creado a través de su id y guarda el User
+*/
+
+
 }
--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,12 +1,23 @@
 package com.example.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     private Long id;
+    private String name;
+    private String username;
+    //isEmailVerified
+    @JsonIgnore
+    private String password;
 
+    //gitLabUsername
+    //countryId --> join
     public void setId(Long id) {
         this.id = id;
     }
